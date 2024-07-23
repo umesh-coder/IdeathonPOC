d285c12527715fda7aefe33e6f8acfa8302fb62f
E5000864 <sagar.arora@innovapptive.com>
2024-07-23 12:29:40 +0530

    result activity speech added

--- app/src/main/java/com/example/ideathonpoc/ui/screens/ResultActivity.kt
+++ app/src/main/java/com/example/ideathonpoc/ui/screens/ResultActivity.kt
@@ -2,6 +2,8 @@ package com.example.ideathonpoc.ui.screens
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ideathonpoc.R
@@ -9,16 +11,32 @@ import com.example.ideathonpoc.databinding.ActivityResultBinding
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.Queue
 
 class ResultActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityResultBinding
+    private lateinit var textToSpeech: TextToSpeech
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        textToSpeech = TextToSpeech(this) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                val result = textToSpeech.setLanguage(Locale.UK)
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Log.e(TAG, "Language not supported")
+                } else {
+                    speakOut()
+                }
+            } else {
+                Log.e(TAG, "Initialization failed")
+            }
+        }
+
+        
 
         val imagePath = intent.getStringExtra("imagePath")
         val timestamp = intent.getLongExtra("timestamp", 0L)
@@ -36,8 +54,21 @@ class ResultActivity : AppCompatActivity() {
             val formattedTime = sdf.format(Date(time))
             binding.timestampTextView.text = "Screenshot taken at: $formattedTime"
         }
+        Log.e(TAG, "onCreate: testing", )
+
     }
 
+    private fun speakOut() {
+        val message = "You wore right PPE, you can proceed with your Job, Stay Vigilant, Stay Safe"
+        textToSpeech.speak(message,TextToSpeech.QUEUE_FLUSH,null,null)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        textToSpeech.stop()
+        textToSpeech.shutdown()
+    }
     companion object {
         private const val TAG = "ResultActivity"
     }
